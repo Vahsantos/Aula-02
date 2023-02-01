e730d7e29aebe49d5771c447856d32b9ce17d87a
Gabriel Lima <gabriel.lyma16@gmail.com>
2023-02-01 19:54:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//EX 1
+console.log('Foram colados: ',(7*30-15))
+
+//EX 2
+console.log('Poderão ser guardadas: ',(2*12*4))
+
+//EX 3
+let numero = 10
+
+numero--
+console.log(numero)
+
+numero=numero-2
+console.log(numero)
+numero=numero-2
+console.log(numero)
+numero=numero-2
+console.log(numero)
+numero=numero-2
+console.log(numero)
